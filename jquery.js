bbaa3ea607d25bcb4d8b93e170f9d9e25f248f84
Ben White <whtouche@gmail.com>
2014-07-18 12:25:06 -0400

    Stopped working on assessment.

--- jquery.js
+++ jquery.js
@@ -3,14 +3,17 @@
 ///// Question 1
 // Make the "Highlight Quotes" link add the class "highlighted" to all quotes
 // within articles (the <q> elements).
+$('#highlight-quotes').click(addClass('highlighted'));
 
 ///// Question 2
 // Make the "Toggle Articles" link hide or show the content, and only the
 // content, of all articles.
+$('#toggle-articles').click(hide('.content'));
 
 ///// Question 3
 // Make the "Close" link in the alert at the top of the page remove the alert
 // from the page entirely (don't just hide it).
+
 
 ///// Question 4
 // Make the comment form functional: Typing something into the input and
@@ -20,3 +23,4 @@
 ///// Question 5
 // Make it so clicking on a comment list item toggles the class "favorite" on
 // it, including comments that were added after the page loaded.
+$('.comments').on('click', 'li', addClass('favorite'));
