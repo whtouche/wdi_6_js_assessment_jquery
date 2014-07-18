3ddb69df1348bff43ecc43436ee39dace0c37c0d
Alex Grant <grantovich@users.noreply.github.com>
2014-07-18 10:43:57 -0400

    Initial version of assessment

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-wdi_6_js_assessment_jquery
-==========================
+# jQuery Assessment
+
+Turn in a pull request with your answers added to `jquery.js`, referring to the markup in `jquery.html` (do not change this file). You may use any resources you like, but answers must be turned in within 20 minutes of the assessment starting.
---
+++ jquery.js
@@ -0,0 +1,22 @@
+// Refer to jquery.html to answer these questions (but do not modify it).
+
+///// Question 1
+// Make the "Highlight Quotes" link add the class "highlighted" to all quotes
+// within articles (the <q> elements).
+
+///// Question 2
+// Make the "Toggle Articles" link hide or show the content, and only the
+// content, of all articles.
+
+///// Question 3
+// Make the "Close" link in the alert at the top of the page remove the alert
+// from the page entirely (don't just hide it).
+
+///// Question 4
+// Make the comment form functional: Typing something into the input and
+// pressing Enter or clicking the Comment button should add a new list item to
+// the list of comments, containing that text.
+
+///// Question 5
+// Make it so clicking on a comment list item toggles the class "favorite" on
+// it, including comments that were added after the page loaded.
