152ab2f87bb38f569bdd22c89a1a27d3a87f98a3
Ben White <whtouche@gmail.com>
2014-11-09 14:54:17 -0500

    cleanup

--- jquery.js
+++ jquery.js
@@ -1,26 +1,49 @@
-// Refer to jquery.html to answer these questions (but do not modify it).
+$(document).ready(function(){
+  ///// Question 1
+  // Make the "Highlight Quotes" link add the class "highlighted" to all quotes
+  // within articles (the <q> elements).
 
-///// Question 1
-// Make the "Highlight Quotes" link add the class "highlighted" to all quotes
-// within articles (the <q> elements).
-$('#highlight-quotes').click(addClass('highlighted'));
+  $('#highlight-quotes').click(function(event){
+    $('article q').addClass('highlighted');
+    event.preventDefault();
+  });
 
-///// Question 2
-// Make the "Toggle Articles" link hide or show the content, and only the
-// content, of all articles.
-$('#toggle-articles').click(hide('.content'));
+  ///// Question 2
+  // Make the "Toggle Articles" link hide or show the content, and only the
+  // content, of all articles.
 
-///// Question 3
-// Make the "Close" link in the alert at the top of the page remove the alert
-// from the page entirely (don't just hide it).
+  $('#toggle-articles').click(function(event){
+    $('article .content').toggle();
+    event.preventDefault();
+  });
 
+  ///// Question 3
+  // Make the "Close" link in the alert at the top of the page remove the alert
+  // from the page entirely (don't just hide it).
 
-///// Question 4
-// Make the comment form functional: Typing something into the input and
-// pressing Enter or clicking the Comment button should add a new list item to
-// the list of comments, containing that text.
+  $('.dismiss-alert').click(function(event){
+    $(this).closest('.alert').remove();
+    event.preventDefault();
+  });
 
-///// Question 5
-// Make it so clicking on a comment list item toggles the class "favorite" on
-// it, including comments that were added after the page loaded.
-$('.comments').on('click', 'li', addClass('favorite'));
+  ///// Question 4
+  // Make the comment form functional: Typing something into the input and
+  // pressing Enter or clicking the Comment button should add a new list item to
+  // the list of comments, containing that text.
+
+  $('.new-comment').submit(function(event){
+    var commentField = $(this).children('.new-comment-text');
+    var newComment = $('<li>').addClass('comment').text(commentField.val());
+    $(this).siblings('.comments').append(newComment);
+    commentField.val('');
+    event.preventDefault();
+  });
+
+  ///// Question 5
+  // Make it so clicking on a comment list item toggles the class "favorite" on
+  // it, including comments that were added after the page loaded.
+
+  $('.comments').on('click', '.comment', function(){
+    $(this).toggleClass('favorite');
+  });
+});
